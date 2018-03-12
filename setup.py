a57704149ba5e8bc1a8dda02d76cea4d5dd1f51a
Dāvis <dmednis@users.noreply.github.com>
2018-03-12 20:07:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '1.0.0',
     url = 'https://github.com/mypackage.git',
     author = 'Davis Mednis',
-    author_email = 'davis@medn.is’,
+    author_email = 'davis@medn.is',
     description = 'This tool allows to introduce controlled degradations to audio.',
     packages = find_packages(),    
     install_requires = ['numpy >= 1.11.1', 'librosa >= 0.6.0'],
